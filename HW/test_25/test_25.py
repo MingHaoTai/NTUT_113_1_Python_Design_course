b078006fe0fe7c7b00995d6837efaf198ac892b5
MingHaoTai <nnn122nnn70@gmail.com>
2024-11-26 00:17:41 +0800

    PD HW 25

--- HW/test_25/test_25.py
+++ HW/test_25/test_25.py
@@ -13,25 +13,29 @@ def bracketsCheck(data : list, depth : int, max_depth : int):
         if data[i] in brackets[0:3]:
             depth += 1
         elif data[i] in brackets[3:6] and brackets[brackets.index(data[i]) - 3] == data[i-1]:
+            if depth > max_depth:
+                max_depth = depth
             depth -= 1
             data.pop(i)
             data.pop(i-1)
-            if bracketsCheck(data, depth, max_depth) == 0:
+            x = bracketsCheck(data, 0, max_depth)
+            if x == 0:
                 return 0
+            else:
+                max_depth = x
             break
         else:
             print('fail')
             return 0
-        if depth > max_depth:
-            max_depth = depth
     if data != []:
         print('fail')
         return 0
-    return max_depth
+    else:
+        return max_depth
 
 def getNumInDepth(s : list, bracketsData : list, max_depth : int):
     brackets = ['(', '[', '{', ')', ']', '}']
-    data = list([None] for i in range(max_depth + 1))
+    data = list([None] for i in range(7))
     j, depth = 0, 0
     for i in range(len(s)):
         if s[i] in brackets[0:3] and  s[i] == bracketsData[j]:
@@ -65,14 +69,11 @@ if __name__ == '__main__':
         dataList = strToList(data[i])
         if dataDepth == 0:
             continue
-        if dataDepth >= findDepth:
-            print('pass,', end='')
-            numData = getNumInDepth(dataList, dataBrackets, dataDepth)
-            if numData[findDepth] == [None]:
-                print('EMPTY')
-            else:
-                for i in range(1, len(numData[findDepth])):
-                    print(numData[findDepth][i], end='')
-                print()
+        print('pass,', end='')
+        numData = getNumInDepth(dataList, dataBrackets, dataDepth)
+        if findDepth > dataDepth or numData[findDepth] == [None]:
+            print('EMPTY')
         else:
-            print('fail')
+            for i in range(1, len(numData[findDepth])):
+                print(numData[findDepth][i], end='')
+            print()
