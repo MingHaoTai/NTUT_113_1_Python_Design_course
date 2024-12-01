ab2d9c890d94052931e226b9215211211959781d
MingHaoTai <nnn122nnn70@gmail.com>
2024-12-01 22:07:48 +0800

    PD HW 26

--- HW/test_26/test_26.py
+++ HW/test_26/test_26.py
@@ -10,6 +10,7 @@ def getData():
             if pack_no[i] == pack_no[j]:
                 print('Duplicated ID')
                 return False
+    for i in range(len(pack_no)-1):
         if int(pack_no[i][0]) > load_limit:
             print('Load limit exceeded')
             return False
@@ -24,14 +25,17 @@ def compute(pack_no : list, load_limit : int):
     sum = 0
     for i in range(len(pack_no)):
         sum += int(pack_no[i][0])
+    if sum % load_limit != 0:
+        print('Cannot be delivered')
+        return False
     N = sum // load_limit
     data = list([] for i in range(N))
     for i in range(N):
         bag = []
         if match(pack_no, load_limit, bag, 0) == False:
             return False
-        print(i)
-        print(bag)
+        # print(i)
+        # print(bag)
         data[i].extend(bag)
         pack_no = remove(pack_no, bag)
     # print(data)
@@ -44,22 +48,30 @@ def output(data : list):
             new_data[i].extend(data[i][j][1])
         new_data[i] = sorted(new_data[i])
     new_data = mySort(new_data)
-    print(new_data)
-    x = []
-    # while True:
-    #     if len(data) == 0:
-    #         break
-    #     for i in range(len(data)-1):
-    #         if len(data[i]) == len(data[i+1]):
-    #             x = data[0:i+2]
-    #         else:
-    #             break
-    #     x.sort()
-    #     for i in range(len(x)):
-    #         for j in range(len(x[i])):
-    #             print(x[i][j], end=' ')    
-    #         print()
-    #         data.remove(x[i])
+    # print(new_data)
+    f(new_data, [], 1)
+
+def f(new_data : list, y : list, length : int):
+    i = 0
+    while True:
+        # print(i)
+        if i < len(new_data) and len(new_data[i]) == length :
+            i += 1
+            continue
+        x = new_data[0:i]
+        x.sort()
+        y.extend(x)
+        # print(y)
+        if i == len(new_data):
+            break
+        else:
+            f(new_data[i:], y, length+1)
+        return
+    print(len(y))
+    for i in range(len(y)):
+        for j in range(len(y[i])):
+            print(y[i][j], end=' ')
+        print()
     
 def mySort(data : list):
     for i in range(len(data)-1):
@@ -76,7 +88,7 @@ def match(pack_no : list, load_limit : int, bag : list, index : int):
         return False
     elif int(pack_no[index][0]) > load_limit:
         print('Cannot be delivered')
-        return False
+        return False 
     elif int(pack_no[index][0]) == load_limit:
         bag.append(pack_no[index])
         return True
