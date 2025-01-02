cb64bb9a201b98b4bd08e4e03df62f864c6f832d
MingHaoTai <nnn122nnn70@gmail.com>
2025-01-03 01:15:01 +0800

    PD HW 38 test

--- HW/test_38/test.py
+++ HW/test_38/test.py
@@ -1,59 +1,45 @@
-from collections import defaultdict, deque
+def getData():
+    n, start, end = map(int, input().split())
+    rest = map(int, input().split())
+    graph = {}
+    for i in range(n):
+        a, b = map(int, input().split())
+        if a not in graph:
+            graph[a] = []
+        if b not in graph:
+            graph[b] = [a]
+        graph[a].append(b)
+        graph[b].append(a)
+    return start, end, rest, graph
 
-def find_shortest_path(N, X, Z, rest_points, roads):
-    # 建立部落圖的鄰接表
-    graph = defaultdict(list)
-    for A, B in roads:
-        graph[A].append(B)
-        graph[B].append(A)
+def findShortest(graph : dict, start : int, end : int):
+    queue = [(start, [start])]
+    visited = []
+    while queue:
+        current, path = queue.pop(0)
+        if current == end:
+            return path
+        if current not in visited:
+            visited.append(current)
+            for i in graph.get(current, []):
+                if i not in visited:
+                    queue.append((i, path + [i]))
+    return None
 
-    # 定義 BFS 搜尋
-    def bfs(start, target, must_visit):
-        queue = deque([(start, [start])])
-        visited = set()
-        while queue:
-            current, path = queue.popleft()
-            if current == target and must_visit.issubset(path):
-                return path
-            if current not in visited:
-                visited.add(current)
-                for neighbor in graph[current]:
-                    if neighbor not in visited:
-                        queue.append((neighbor, path + [neighbor]))
-        return None
-
-    # 找從 X 經過某個休息點到 Z 的最短路徑
+if __name__ == '__main__':
+    start, end, restStop, graph = getData()
     shortest_path = None
-    shortest_rest_point = None
-    for rest_point in rest_points:
-        path = bfs(X, Z, {rest_point})
-        if path and (shortest_path is None or len(path) < len(shortest_path)):
-            shortest_path = path
-            shortest_rest_point = rest_point
-
-    return shortest_rest_point, shortest_path
-
-# 輸入處理
-if __name__ == "__main__":
-    # 第一行輸入
-    first_line = input().strip().split()
-    N, X, Z = map(int, first_line)
-
-    # 第二行輸入
-    rest_points = set(map(int, input().strip().split()))
-
-    # 第三行到第 N+2 行輸入
-    roads = []
-    for _ in range(N):
-        A, B = map(int, input().strip().split())
-        roads.append((A, B))
-
-    # 計算結果
-    rest_point, path = find_shortest_path(N, X, Z, rest_points, roads)
-
-    # 輸出結果
-    if path:
-        print(rest_point)
-        print(" ".join(map(str, path)))
+    rest = None
+    for i in restStop:
+        start_to_rest = findShortest(graph, start, i)
+        rest_to_end = findShortest(graph, i, end)
+        if start_to_rest != None and rest_to_end != None:
+            start_to_end = start_to_rest[:-1] + rest_to_end
+            if shortest_path == None or len(start_to_end) < len(shortest_path):
+                shortest_path = start_to_end
+                rest = i
+    if shortest_path != None:
+        print(rest)
+        print(' '.join(map(str, shortest_path)))
     else:
-        print("NO")
+        print('NO')
